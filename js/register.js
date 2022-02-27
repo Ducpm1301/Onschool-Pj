afebaccdea956f613d9447c1384f361d6e3f8c00
Ducpm1301 <99703143+Ducpm1301@users.noreply.github.com>
2022-02-27 10:27:09 +0700

    Commit

--- js/register.js
+++ js/register.js
@@ -9,10 +9,9 @@ $(document).ready(function(){
         $(this).css({"text-decoration":"underline","cursor":"pointer"});
         }, function(){
         $(this).css("text-decoration", "none");
-      });
+    });
 
                                         //FORM VALIDATION
-                                        
     $(".formValidation").validate({
         rules:{
             studentId : 'required',
@@ -21,17 +20,33 @@ $(document).ready(function(){
             lname: 'required',
             birth: 'required',
             birthPlace: 'required',
-            email: 'required',
+            race: 'required',
+            religion: 'required',
+            citizenId: 'required',
+            noicap: 'required',
+            email: {
+                required: true,
+                email: true
+            },
+            address:'true'
         },
         messages: {
-            studentId:'<small style ="color:red"><em>* Hãy điền mã sinh viên vào ô trống *</em></small>',
-            fileId:'<small style ="color:red"><em>* Hãy điền mã hồ sơ vào ô trống *</em></small>',
-            fname: '<small style ="color:red"><em>* Hãy điền họ và tên đệm *</em></small>',
-            lname:'<small style ="color:red"><em>* Hãy điền tên *</em></small>',
+            studentId:'<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+            fileId:'<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+            fname: '<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+            lname:'<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
             birth:'<small style ="color:red"><em>* Hãy chọn ngày tháng năm sinh *</em></small>',
-            birthPlace:'<small style ="color:red"><em>* Hãy điền nơi sinh *</em></small>',
-            email: '<small style ="color:red"><em>* Hãy điền email *</em></small>',
-        }
+            birthPlace:'<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+            race:  '<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+            religion: '<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+            citizenId: '<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+            noicap: '<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+            email:{
+                required: '<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>',
+                email:  '<small style ="color:red"><em>* Hãy điền đúng định dạng email *</em></small>',
+            },
+            address:'<small style ="color:red"><em>* Hãy điền thông tin vào ô trống *</em></small>'
+        },
     })
 
                                 //Gender select box validation
@@ -39,9 +54,14 @@ $(document).ready(function(){
     $(".submitBtn").click(function(){
         if ($('#gender').val() === ""){
             $('#errorMsg').html("<small style ='color:red'><em>* Hãy chọn giới tính của bạn *</em></small>");
+        }else {
+                $('#errorMsg').text('');
         }
-        else {
-            $('#errorMsg').text('');
+    },function(){
+        if ($('#status').val() === ""){
+            $('#errorMsg2').html("<small style ='color:red'><em>* Hãy chọn trạng thái*</em></small>");
+        }else {
+                $('#errorMsg2').text('');
         }
-    })
-});
+    });
+})
